0920b79147de46db26e3dec0c3e04d73d493f209
prathmesh0125 <bidveprathmesh590@gmail.com>
2024-06-22 15:26:14 +0530

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import FeturedSection from "@/components/FeturedSection";
+import Footer from "@/components/Footer";
 import Herosection from "@/components/Herosection";
+import Instructors from "@/components/Instructor";
+import MusicSchoolTestimonials from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Whychooseus from "@/components/Whychooseus";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white/[0.96] antialiased text-black bg-grid-white/[0.02] dark:bg-black dark:text-white">
    
       <Herosection/>
+      <FeturedSection/>
+      <Whychooseus/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
